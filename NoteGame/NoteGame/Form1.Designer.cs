29012007faadb1235e0615eb7564e34ae58954c4
Bailey <Bailey@BAILEYPC>
2016-11-10 00:13:33 -0500

    Left off on case 6 A key, case 5 win/lose unreachable

---
+++ NoteGame/NoteGame/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace NoteGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.outcomeLabel = new System.Windows.Forms.Label();
+            this.aLabel = new System.Windows.Forms.Label();
+            this.bLabel = new System.Windows.Forms.Label();
+            this.cLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.Transparent;
+            this.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputLabel.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(13, 20);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(359, 124);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.Text = "You\'re sitting in a coffee shop when a stranger walks by and drops a folded note " +
+    "on your table.";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Courier New", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(13, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "A";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Courier New", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(13, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "B";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Courier New", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Gold;
+            this.label3.Location = new System.Drawing.Point(13, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 30);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "C";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // outcomeLabel
+            // 
+            this.outcomeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.outcomeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outcomeLabel.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outcomeLabel.Location = new System.Drawing.Point(13, 348);
+            this.outcomeLabel.Name = "outcomeLabel";
+            this.outcomeLabel.Size = new System.Drawing.Size(359, 100);
+            this.outcomeLabel.TabIndex = 4;
+            // 
+            // aLabel
+            // 
+            this.aLabel.BackColor = System.Drawing.Color.Transparent;
+            this.aLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aLabel.Location = new System.Drawing.Point(49, 175);
+            this.aLabel.Name = "aLabel";
+            this.aLabel.Size = new System.Drawing.Size(323, 40);
+            this.aLabel.TabIndex = 5;
+            this.aLabel.Text = "Open the note and read it.";
+            // 
+            // bLabel
+            // 
+            this.bLabel.BackColor = System.Drawing.Color.Transparent;
+            this.bLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bLabel.Location = new System.Drawing.Point(49, 220);
+            this.bLabel.Name = "bLabel";
+            this.bLabel.Size = new System.Drawing.Size(323, 40);
+            this.bLabel.TabIndex = 6;
+            this.bLabel.Text = "Brush the note off your table.";
+            // 
+            // cLabel
+            // 
+            this.cLabel.BackColor = System.Drawing.Color.Transparent;
+            this.cLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cLabel.Location = new System.Drawing.Point(49, 265);
+            this.cLabel.Name = "cLabel";
+            this.cLabel.Size = new System.Drawing.Size(323, 40);
+            this.cLabel.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 325);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Outcome:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 466);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cLabel);
+            this.Controls.Add(this.bLabel);
+            this.Controls.Add(this.aLabel);
+            this.Controls.Add(this.outcomeLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Note";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label outcomeLabel;
+        private System.Windows.Forms.Label aLabel;
+        private System.Windows.Forms.Label bLabel;
+        private System.Windows.Forms.Label cLabel;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ NoteGame/NoteGame/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoteGame
+{
+    public partial class Form1 : Form
+    {
+        Random rnd;
+        int scene = 1;
+        int chance;
+        int ladder = 0;
+        bool front, back, /*ladderBreak,*/ contFlee, hatch;
+        string prompt = "What do you do?";
+        public Form1()
+        {
+            InitializeComponent();
+            rnd = new Random();
+            chance = rnd.Next(1, 101);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            aLabel.Text = "";
+            bLabel.Text = "";
+            cLabel.Text = "";
+
+            if (e.KeyCode == Keys.A)
+            {
+                switch (scene)
+                {
+                    case 1:
+                        outcomeLabel.Text = "The note reads: We are discovered, flee immediately.";
+                        outputLabel.Text = prompt;
+                        scene = 2;
+                        break;
+                    case 2:
+                        outcomeLabel.Text = "A black SUV screeches around the corner to your left down the street.";
+                        front = true;
+                        back = false;
+                        scene = 3;
+                        break;
+                    case 3:
+                        if (front == true)
+                        {
+                            //restart button
+                            outcomeLabel.Text = "The SUV speeds down to you, the window rolls down and a gun barrel is revealed. Darkness.";
+                            outcomeLabel.ForeColor = Color.White;
+                            outcomeLabel.BackColor = Color.Red;
+                        }
+                        else if (back == true)
+                        {
+                            outcomeLabel.Text = "You reach the top and look back, the man is climbing the ladder after you.";
+                            scene = 4;
+                        }
+                        break;
+                    case 4:
+                        if (front == true)
+                        {
+                            outcomeLabel.Text = "You hear men yelling from the shop, you turn and see them running towards you, firearms in hand. Citizens panic and create a temporary diversion.";
+                            outputLabel.Text = "";
+                            scene = 5;
+                        }
+                        else if (back == true)
+                        {
+                            if (chance >= 1 && chance < 31)
+                            {
+                                outcomeLabel.Text = "Fail, turn and hop to the next rooftop, the man in close pursuit";
+                                ladder = 2;
+                                scene = 5;
+                            }
+                            else if (chance >= 31 && chance < 101)
+                            {
+                                outcomeLabel.Text = "Succeed, the man falls off, knocking him unconcious";
+                                //ladderBreak = true;
+                                ladder = 1;
+                                scene = 5;
+                            }
+                        }
+                        break;
+                    case 5:                                                                             //SCENARIOS UNREACHABLE
+                        if (front == true && chance >= 1 && chance <= 50)
+                        {
+                            //restart button
+                            outcomeLabel.Text = "Escape with the crowd to safety. YOU WIN!";
+                            outcomeLabel.BackColor = Color.Gold;
+                            outcomeLabel.ForeColor = Color.White;
+                        }
+                        else if (back == true && chance >= 51 && chance <= 101)
+                        {
+                            //restart button
+                            outcomeLabel.Text = "You see a glare in a window from a building across the street. BANG.";
+                            outcomeLabel.ForeColor = Color.White;
+                            outcomeLabel.BackColor = Color.Red;
+                        }
+                        else if (back == true && ladder == 0)
+                        {
+                            outcomeLabel.Text = "You land in an empty office floor, a custodian closet beside you and a stairwell nearby";
+                            scene = 6;
+                        }
+                        break;
+                }
+            }
+            if (e.KeyCode == Keys.B)
+            {
+                switch (scene)
+                {
+                    case 1:
+                        //restart button
+                        outcomeLabel.Text = "You hear tires screeching outside. The pattering of dress shoes enter the shop. You feel a knock to the head. Darkness.";
+                        outcomeLabel.ForeColor = Color.White;
+                        outcomeLabel.BackColor = Color.Red;
+                        outputLabel.Text = "";
+                        break;
+                    case 2:
+                        //restart button
+                        outcomeLabel.Text = "You hear tires screeching outside. The pattering of dress shoes enter the shop. You feel a knock to the head. Darkness.";
+                        outcomeLabel.ForeColor = Color.White;
+                        outcomeLabel.BackColor = Color.Red;
+                        outputLabel.Text = "";
+                        break;
+                    case 3:
+                        if (front == true)
+                        {
+                            outcomeLabel.Text = "You hide underneath the picnic bench.";  
+                            outputLabel.Text = "The SUV stops in front of the shop, men in suits rush into the building.";
+                            scene = 4;
+                        }
+                        else if (back == true)
+                        {
+                            //restart button
+                            outcomeLabel.Text = "The man pulls a gun from his inner jacket and aims. Darkness.";
+                            outcomeLabel.ForeColor = Color.White;
+                            outcomeLabel.BackColor = Color.Red;
+                        }
+                        break;
+                    case 4:
+                        if (front == true)
+                        {
+                            //restart button
+                            outcomeLabel.Text = "The men come out of the shop and begin to search the area. The men quickly see you and drag you to the SUV. They put a bag over your head. Darkness";
+                            outcomeLabel.ForeColor = Color.White;
+                            outcomeLabel.BackColor = Color.Red;
+                            outputLabel.Text = "";
+                        }
+                        else if (back == true && ladder == 2)
+                        {
+                            outcomeLabel.Text = "You look behind you. The man says to his earpiece \"target in open sight\" You hear a loud gunshot. Darkness";
+                            outcomeLabel.ForeColor = Color.White;
+                            outcomeLabel.BackColor = Color.Red;
+                        }
+                        else if (back == true && ladder == 0)
+                        {
+                            outcomeLabel.Text = "There's a hatch on the roof, there's nowhere else to go.";
+                            hatch = true;
+                            scene = 5;
+                        }                        
+                        break;
+                    case 5:
+                        if (front == true && chance >= 1 && chance <= 30)
+                        {
+                            //restart button
+                            outcomeLabel.Text = "Hide with the restaurant staff, you're never found. YOU WIN!";
+                            outcomeLabel.BackColor = Color.Gold;
+                            outcomeLabel.ForeColor = Color.White;
+                        }
+                        else if (back == true && chance >= 31 && chance <= 101)
+                        {
+                            //restart button
+                            outcomeLabel.Text = "The men in suits follow you to the restaurant and take you into their custody.";
+                            outcomeLabel.ForeColor = Color.White;
+                            outcomeLabel.BackColor = Color.Red;
+                        }
+                        else if (back == true && hatch == true)
+                        {
+                            outcomeLabel.Text = "Open the roof hatch and drop in"; //60/40
+                        }
+                        break;
+                }
+            }
+            if (e.KeyCode == Keys.C)
+            {
+                switch (scene)
+                {
+                    case 1:                                           
+                        break;
+                    case 2:
+                        outcomeLabel.Text = "A man in a suit down the alley talks into an earpiece and moves quickly towards you.";
+                        outputLabel.Text = prompt;
+                        back = true;
+                        front = false;
+                        scene = 3;
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                }
+            }
+
+            //SCENE OUTPUT AND CHOICES
+            switch (scene)
+            {
+                case 1:
+                    break;
+                case 2:
+                    aLabel.Text = "Run out the front door.";
+                    bLabel.Text = "Ignore the note. Crazy nonsense.";
+                    cLabel.Text = "Run out the back door to the alley.";
+                    break;
+                case 3:
+                    if (front == true)
+                    {
+                        outputLabel.Text = "";
+                        aLabel.Text = "Run in the opposite direction down the street.";
+                        bLabel.Text = "Duck under a picnic bench nearby.";
+                        cLabel.Text = "";
+                    }
+                    else if (back == true)
+                    {
+                        outputLabel.Text = "";
+                        aLabel.Text = "Climb a ladder to the shop's rooftop.";
+                        bLabel.Text = "Run down the alley away from the man.";
+                        cLabel.Text = "";
+                    }                   
+                    break;
+                case 4:
+                    if (front == true)
+                    {
+                        aLabel.Text = "Leave the bench, running down the street.";
+                        bLabel.Text = "Stay under the bench.";
+                        cLabel.Text = "";
+                    }
+                    else if (back == true)
+                    {
+                        aLabel.Text = "Try to shake the ladder loose off the wall."; //30/70
+                        bLabel.Text = "Continue to flee, hop to the next rooftop.";
+                        cLabel.Text = "";
+                        hatch = true;
+                    }
+                    break;
+                case 5:
+                    if (front == true)
+                    {
+                        aLabel.Text = "Take a man's ball cap and put it on and run with the crowd."; //50/50
+                        bLabel.Text = "Run into a nearby restraunt."; //30/70
+                        cLabel.Text = "";
+                    }
+                    else if (back == true && ladder == 0)   //ladderchange
+                    {
+                        aLabel.Text = "Open the roof hatch and drop in"; //60/40 
+                        bLabel.Text = "";
+                        cLabel.Text = "";
+                    }
+                    else if (back == true && ladder == 1)
+                    {
+                        aLabel.Text = "Run and jump to the next rooftop";
+                    }
+                    break;
+            }
+        }
+    }
+}
+// LEFT OFF ON CASE 6                       
